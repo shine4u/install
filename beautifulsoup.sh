d802ef0b65cc93b21cd52b24196f66e53eae918d
shine4u <shine19890705@gmail.com>
2014-02-10 10:51:26 +0800

    Create beautifulsoup.sh

---
+++ beautifulsoup.sh
@@ -0,0 +1,4 @@
+wget http://www.crummy.com/software/BeautifulSoup/bs4/download/4.3/beautifulsoup4-4.3.2.tar.gz
+tar xzvf beautifulsoup4-4.3.2.tar.gz 
+cd beautifulsoup4-4.3.2
+sudo python setup.py install 
