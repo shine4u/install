cd8b44c831418ec5433fe160e1a182df71672a61
shine4u <shine19890705@gmail.com>
2014-06-13 10:40:15 +0800

    Update python.sh

--- python.sh
+++ python.sh
@@ -2,3 +2,4 @@ wget https://www.python.org/ftp/python/2.7.7/Python-2.7.7.tgz --no-check-certifi
 tar xvf Python-2.7.7.tgz 
 cd Python-2.7.7
  ./configure --prefix=/home/spider/wangbangxu/install/ # modify if need.
+make && make install
