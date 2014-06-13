2746118e2d85efc6f8fae4011139286917795250
shine4u <shine19890705@gmail.com>
2014-06-13 10:59:54 +0800

    Update mysqldb.sh

--- mysqldb.sh
+++ mysqldb.sh
@@ -1,2 +1,4 @@
 wget --no-check-certificate https://bootstrap.pypa.io/ez_setup.py -O - | bin/python  # attention for python path
-
+wget http://sourceforge.net/projects/mysql-python/files/mysql-python/1.2.3/MySQL-python-1.2.3.tar.gz
+tar xzvf MySQL-python-1.2.3.tar.gz && cd MySQL-python-1.2.3
+./../bin/python setup.py  install
