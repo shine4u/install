9087cb65f425b6b703083bdc6eb4a2241477d3f1
shine4u <shine19890705@gmail.com>
2014-06-13 10:30:50 +0800

    Create python.sh

---
+++ python.sh
@@ -0,0 +1,4 @@
+wget https://www.python.org/ftp/python/2.7.7/Python-2.7.7.tgz --no-check-certificate
+tar xvf Python-2.7.7.tgz 
+cd Python-2.7.7
+ ./configure --prefix=/home/spider/wangbangxu/install/ # modify if need.
