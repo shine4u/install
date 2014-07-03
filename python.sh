833237ac4f47b4e5d2789852299795aa9613953e
shine4u <shine19890705@gmail.com>
2014-07-03 10:36:11 +0800

    Update python.sh

--- python.sh
+++ python.sh
@@ -6,7 +6,7 @@ yum -y install sqlite-devel.x86_64
 
 yum -y install openssl-devel.x86_64
 
-
+# 添加新的package后不需要重新configure，直接make即可
  
 wget https://www.python.org/ftp/python/2.7.7/Python-2.7.7.tgz --no-check-certificate
 tar xvf Python-2.7.7.tgz 
