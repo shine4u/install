4453152ec6843d9305fea4235067d9cf3e5847a6
shine4u <shine19890705@gmail.com>
2014-07-03 10:35:30 +0800

    Update python.sh

--- python.sh
+++ python.sh
@@ -1,3 +1,13 @@
+
+yum -y install zlib.x86_64
+yum -y install zlib-devel.x86_64
+
+yum -y install sqlite-devel.x86_64 
+
+yum -y install openssl-devel.x86_64
+
+
+ 
 wget https://www.python.org/ftp/python/2.7.7/Python-2.7.7.tgz --no-check-certificate
 tar xvf Python-2.7.7.tgz 
 cd Python-2.7.7
